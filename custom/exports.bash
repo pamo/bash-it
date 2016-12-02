cdb068942f87f7bb256730cd1c09bf4fa6e58550
Pamela Ocampo <pamela.ocampo@gmail.com>
2016-12-01 16:23:51 -0800

    Adds baseline for custom bash-it settings

---
+++ custom/exports.bash
@@ -0,0 +1,44 @@
+# Set the default editor
+if [ -f /usr/local/bin/subl ]; then
+				export EDITOR='subl -w'
+else
+				export EDITOR="vim"
+fi
+
+# no ._ files in archives please
+export COPYFILE_DISABLE=true
+
+export NODE_PATH=/usr/local/lib/node_modules:$NODE_PATH
+
+# Larger bash history (allow 32³ entries; default is 500)
+export HISTSIZE=32768
+export HISTFILESIZE=$HISTSIZE
+export HISTCONTROL=ignoredups
+# Make some commands not show up in history
+export HISTIGNORE="ls:cd:cd -:pwd:exit:date:* --help"
+
+# Prefer US English and use UTF-8
+export LANG="en_US"
+export LC_ALL="en_US.UTF-8"
+
+# Highlight section titles in manual pages
+export LESS_TERMCAP_md="$ORANGE"
+
+# Don’t clear the screen after quitting a manual page
+export MANPAGER="less -X"
+
+# Always enable colored `grep` output
+export GREP_OPTIONS="--color=auto"
+
+# Link Homebrew casks in `/Applications` rather than `~/Applications`
+export HOMEBREW_CASK_OPTS="--appdir=/Applications"
+export PATH="/usr/local/sbin:$PATH"
+
+export JBOSS_HOME=/usr/local/opt/jboss-as/libexec
+export PATH=${PATH}:${JBOSS_HOME}/bin
+
+export VIRTUALENV_DISTRIBUTE=true
+export PIP_VIRTUALENV_BASE=$HOME/.virtualenvs
+export PIP_REQUIRE_VIRTUALENV=true
+export PIP_DOWNLOAD_CACHE=$HOME/.pip/cache
+
