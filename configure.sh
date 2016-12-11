937995f0f4e8f09ccc5563cad627f7eb9f6a7b01
Pamela Ocampo <pamela.ocampo@gmail.com>
2016-12-10 21:39:10 -0800

    Formatting

--- configure.sh
+++ configure.sh
@@ -1,60 +1,60 @@
 #!/usr/bin/env bash
-export BASH_IT="$HOME/.bash_it"
+export BASH_IT="$HOME/.bash-it"
 source $BASH_IT/bash_it.sh
 
 completions=(
-	bash-it               
-	brew                  
-	bundler               
-	defaults              
-	dirs                  
-	gem                   
-	gh                    
-	git                   
-	gulp                  
-	npm                   
-	rake                  
-	ssh                   
-	tmux                  
-	todo                  
+  bash-it
+  brew
+  bundler
+  defaults
+  dirs
+  gem
+  gh
+  git
+  gulp
+  npm
+  rake
+  ssh
+  tmux
+  todo
 )
 bash-it enable completion ${completions[@]}
 
 aliases=(
-	bundler
-	clipboard
-	fuck
-	git
-	heroku
-	homebrew
-	homebrew-cask
-	osx
-	rails
-	tmux
-	todo
-	vim
+  bundler
+  clipboard
+  fuck
+  git
+  heroku
+  homebrew
+  homebrew-cask
+  osx
+  rails
+  tmux
+  todo
+  vim
 )
 bash-it enable alias ${aliases[@]}
 
 plugins=(
-	alias-completion
-	base
-	battery
-	dirs
-	explain
-	extract
-	gif
-	git-subrepo
-	git
-	history
-	node
-	osx-timemachine
-	osx
-	rbenv
-	ruby
-	ssh
-	tmux
-	tmuxinator
-	xterm
+  alias-completion
+  base
+  battery
+  dirs
+  explain
+  extract
+  gif
+  git-subrepo
+  git
+  history
+  node
+  osx-timemachine
+  osx
+  rbenv
+  ruby
+  ssh
+  tmux
+  tmuxinator
+  xterm
 )
 bash-it enable plugin ${plugins[@]}
