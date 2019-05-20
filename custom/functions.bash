482fbfc741c5da6fd6abfd8c03cf16da57cc50c4
Pamela Ocampo <pamela.ocampo@gmail.com>
2019-05-20 08:50:16 -0700

    Add change mac addr fn for wifi

--- custom/functions.bash
+++ custom/functions.bash
@@ -377,3 +377,11 @@ function gitf (){
 
   git checkout -b feature/${1}
 }
+
+function changeMac() {
+    local mac=$(openssl rand -hex 6 | sed 's/\(..\)/\1:/g; s/.$//')
+    sudo ifconfig en0 ether $mac
+    sudo ifconfig en0 down
+    sudo ifconfig en0 up
+    echo "Your new physical address is $mac"
+}
